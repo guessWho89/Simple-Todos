169c73e2cd05e29df76f840d2a52d2c78834e1a7
Aco_M <aleksandarmilosevic89@gmail.com>
2020-07-16 12:38:06 +0200

    update

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const cacheAssets = [
 
 // Call install event 
 self.addEventListener('install', (e) => {
-    console.log('sw: installed');
+    // console.log('sw: installed');
     e.waitUntil(
         caches
             .open(cacheName)
@@ -19,8 +19,7 @@ self.addEventListener('install', (e) => {
 
 // Call activate event
 self.addEventListener('activate', (e) => {
-    console.log('sw: activated');
-
+    // console.log('sw: activated');
     // Remove unwanted caches
     e.waitUntil(
         caches.keys().then(cacheNames => {
@@ -38,7 +37,7 @@ self.addEventListener('activate', (e) => {
 
 // Call fetch event 
 self.addEventListener('fetch', (e) => {
-    console.log('sw: fetching');
+    // console.log('sw: fetching');
     e.respondWith(fetch(e.request)
         .then(res => {
             // Make clone of response
